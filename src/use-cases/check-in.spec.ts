6560ac52bd7396a3d863acc3d0c8d2037d75633e
mateus-cristian <mateus_cristian@outlook.com>
2024-02-05 22:13:29 -0300

    fix: remove imports

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -1,7 +1,4 @@
 import { describe, it, expect, beforeEach } from 'vitest'
-import { RegisterUseCase } from './register'
-import { compare } from 'bcryptjs'
-import { UserAlreadyExistsError } from './errors/user-already-exists'
 import { CheckInUseCase } from './checkIn'
 import { InMemoryCheckInRepository } from '@/repositories/in-memory/in-memory-check-in-repository'
 
--- src/use-cases/checkIn.ts
+++ src/use-cases/checkIn.ts
@@ -1,7 +1,4 @@
-import { UsersRepository } from '@/repositories/users-repository'
-import { InvalidCredencialsError } from './errors/invalid-credentials-error'
-import { compare } from 'bcryptjs'
-import { CheckIn, User } from '@prisma/client'
+import { CheckIn } from '@prisma/client'
 import { CheckInsRepository } from '@/repositories/check-in-repository'
 
 interface CheckInUseCaseRequest {
