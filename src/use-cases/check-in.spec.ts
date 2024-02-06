8acf7cace40bb9ce917a49a41c6d803ee048c374
mateus-cristian <mateus_cristian@outlook.com>
2024-02-05 22:12:48 -0300

    feat:checkIn test,use case

---
+++ src/use-cases/check-in.spec.ts
@@ -0,0 +1,25 @@
+import { describe, it, expect, beforeEach } from 'vitest'
+import { RegisterUseCase } from './register'
+import { compare } from 'bcryptjs'
+import { UserAlreadyExistsError } from './errors/user-already-exists'
+import { CheckInUseCase } from './checkIn'
+import { InMemoryCheckInRepository } from '@/repositories/in-memory/in-memory-check-in-repository'
+
+let checkInRepository: InMemoryCheckInRepository
+let sut: CheckInUseCase
+
+describe('CheckIn Use Case', () => {
+  beforeEach(() => {
+    checkInRepository = new InMemoryCheckInRepository()
+    sut = new CheckInUseCase(checkInRepository)
+  })
+
+  it('should be able to check in', async () => {
+    const { checkIn } = await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+    })
+
+    expect(checkIn.user_id).toEqual(expect.any(String))
+  })
+})
