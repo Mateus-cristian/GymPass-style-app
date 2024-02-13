fccde3b68026a71e597efa4fc7fc543a6d499965
mateus-cristian <mateus_cristian@outlook.com>
2024-02-12 22:31:14 -0300

    feat:separate controllers

---
+++ src/http/controllers/gyms/nearby.ts
@@ -0,0 +1,27 @@
+import { makeFetchNearbyGymsUseCase } from '@/use-cases/factories/make-fetch-nearby-gyms-use-case'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function nearby(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    latitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90
+    }),
+    longitude: z.number().refine((value) => {
+      return Math.abs(value) <= 180
+    }),
+  })
+
+  const { latitude, longitude } = registerBodySchema.parse(request.body)
+
+  const nearbyGymUseCase = makeFetchNearbyGymsUseCase()
+
+  const { gyms } = await nearbyGymUseCase.execute({
+    userLatitude: latitude,
+    userLongitude: longitude,
+  })
+
+  return reply.status(201).send({
+    gyms,
+  })
+}
